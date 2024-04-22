6225deaadd9e89cf73815778b6796ff8ec44e5cd
itisameerkhan <itisameerkhan@gmail.com>
2024-04-22 19:36:06 +0530

    controller function updated

--- Backend/controllers/dashboardController.js
+++ Backend/controllers/dashboardController.js
@@ -1,9 +1,34 @@
-export const createBMS = (req, res) => {
-  res.json({
-    success: true,
-    status: 200,
-    data: "some data",
-    message: "data created successfully",
-    errorMessage: "no error",
-  });
+import Dashboard from "../models/Dashboard.js";
+
+export const createBMS = async (req, res, next) => {
+  try {
+    const { temperature, humidity, voltage, batteryPercentage } = req.body;
+    if (!temperature || !humidity || !voltage || !batteryPercentage) {
+      throw new Error("all field required");
+    }
+    const mongoResults = await Dashboard.find({
+      temperature,
+      humidity,
+      voltage,
+    });
+
+    if (mongoResults.length > 0) {
+      throw new Error("Data Already Existed in our Database");
+    }
+    const response = await Dashboard.create({
+      humidity,
+      temperature,
+      voltage,
+      batteryPercentage,
+    });
+
+    res.json({
+      success: true,
+      status: 200,
+      data: response,
+      message: "data created successfully",
+    });
+  } catch (e) {
+    next(e);
+  }
 };
