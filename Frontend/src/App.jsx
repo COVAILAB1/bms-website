58a07f7fb73c61966dfb77db9114ce7726bd1737
itisameerkhan <itisameerkhan@gmail.com>
2024-04-24 14:33:44 +0530

    emailjs added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setData } from "../redux/dataSlice.js";
 import Loader from "./Components/Loader/Loader";
+import emailjs from "@emailjs/browser";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -29,6 +30,29 @@ const App = () => {
     const mongoData = await axios.get("http://localhost:8080/api/getdata");
     dispatch(setData(mongoData.data.data));
     setBmsdata(mongoData.data.data);
+    console.log(mongoData.data.data[mongoData.data.data.length - 1]);
+
+    const templateParams = {
+      from_name: "SmartEV Team",
+      from_email: "smartevteam@example.in",
+      to_name: "Ameer khan B",
+      temperature:
+        mongoData.data.data[mongoData.data.data.length - 1].temperature,
+      voltage: mongoData.data.data[mongoData.data.data.length - 1].voltage,
+      humidity: mongoData.data.data[mongoData.data.data.length - 1].humidity,
+      batteryPercentage:
+        mongoData.data.data[mongoData.data.data.length - 1].batteryPercentage,
+    };
+
+    console.log("template", templateParams);
+    console.log("email js called");
+    const emailResponse = await emailjs.send(
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      templateParams,
+      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+    );
+    console.log(emailResponse);
   };
 
   useEffect(() => {
