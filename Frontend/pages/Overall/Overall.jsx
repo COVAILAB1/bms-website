bcdd467b35ddc1c8181facc5266bf0880bf8ad05
itisameerkhan <itisameerkhan@gmail.com>
2024-04-24 21:07:19 +0530

    fix

--- Frontend/pages/Overall/Overall.jsx
+++ Frontend/pages/Overall/Overall.jsx
@@ -1,67 +1,25 @@
 import { useSelector } from "react-redux";
 import "./Overall.scss";
 import { LineChart } from "@mui/x-charts/LineChart";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Loader from "../../src/Components/Loader/Loader";
 
-const Overall = () => {
-  // const data = useSelector((store) => store.bmsData.data);
+const Overall = () => { //change graphdata
+  const data = useSelector((store) => store.bmsData.data);
 
-  // const [graphdata, setGraphdata] = useState(data.slice(-10));
-  const [data, setData] = useState();
+  const [graphData, setGraphData] = useState([]);
 
-  const getData = async () => {
-    const response = await axios.get(
-      `https://blr1.blynk.cloud/external/api/getAll?token=${
-        import.meta.env.VITE_BLYNK_TOKEN
-      }`
-    );
-    console.log("response", response);
-    const mongoDataPost = await axios.post(
-      "https://bms-website-server.onrender.com/api/create-bms-data",
-      {
-        temperature: response.data.v1,
-        humidity: response.data.v2,
-        voltage: response.data.v3,
-        batteryPercentage: response.data.v4,
-      }
-    );
-    const mongoData = await axios.get(
-      "https://bms-website-server.onrender.com/api/getdata"
-    );
-    // dispatch(setData(mongoData.data.data));
-    setData(mongoData.data.data);
-    console.log(mongoData.data.data[mongoData.data.data.length - 1]);
+  if(!Object.keys(data).length === 0) {
+    setGraphData(data.slice(-10));
+  }
+  // if (Object.keys(data).length === 0) {
+  //   navigate("/");
+  // }
 
-    const templateParams = {
-      from_name: "SmartEV Team",
-      from_email: "smartevteam@example.in",
-      to_name: "Ameer khan B",
-      temperature:
-        mongoData.data.data[mongoData.data.data.length - 1].temperature,
-      voltage: mongoData.data.data[mongoData.data.data.length - 1].voltage,
-      humidity: mongoData.data.data[mongoData.data.data.length - 1].humidity,
-      batteryPercentage:
-        mongoData.data.data[mongoData.data.data.length - 1].batteryPercentage,
-    };
+  // const [graphdata, setGraphdata] = useState(data.slice(-10))
 
-    console.log("template", templateParams);
-    console.log("email js called");
-    // const emailResponse = await emailjs.send(
-    //   import.meta.env.VITE_EMAILJS_SERVICE_ID,
-    //   import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-    //   templateParams,
-    //   import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-    // );
-    // console.log(emailResponse);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  if(!data) return <Loader />
+  if(Object.keys(data).length === 0) return <Loader />
 
   return (
     <div className="overall-main">
@@ -90,7 +48,7 @@ const Overall = () => {
           xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
           series={[
             {
-              data: data.map((data) => data.temperature),
+              data: graphData.map((data) => data.temperature), 
             },
           ]}
           height={300}
@@ -102,7 +60,7 @@ const Overall = () => {
           xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
           series={[
             {
-              data: data.map((data) => data.humidity),
+              data: graphData.map((data) => data.humidity),
             },
           ]}
           height={300}
@@ -114,7 +72,7 @@ const Overall = () => {
           xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
           series={[
             {
-              data: data.map((data) => data.voltage),
+              data: graphData.map((data) => data.voltage),
             },
           ]}
           height={300}
@@ -126,7 +84,7 @@ const Overall = () => {
           xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
           series={[
             {
-              data: data.map((data) => data.batteryPercentage),
+              data: graphData.map((data) => data.batteryPercentage),
             },
           ]}
           height={300}
